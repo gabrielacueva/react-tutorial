acd12fbafa000093267ceabb447343fe4bcf0ccc
gabriela cueva <gabriela@datank.ai>
2020-01-20 17:12:18 -0700

    Updates

---
+++ precache-manifest.bc5b539413fdf7d19058b1856cb701aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a609014247cd62f255c6d63f05b14e",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "7a9692d9757230248f6e",
+    "url": "/react-tutorial/static/css/main.629b192f.chunk.css"
+  },
+  {
+    "revision": "28f88d6e7bf2a5d74419",
+    "url": "/react-tutorial/static/js/2.d35a2c42.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-tutorial/static/js/2.d35a2c42.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a9692d9757230248f6e",
+    "url": "/react-tutorial/static/js/main.058f6376.chunk.js"
+  },
+  {
+    "revision": "d1a4ad59d776d93e8cfb",
+    "url": "/react-tutorial/static/js/runtime-main.4b063f4c.js"
+  }
+]);
